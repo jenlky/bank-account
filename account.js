367090243ddbb98521f5cad466d7be072d014aff
Jenssen Lee <30688276+helplah@users.noreply.github.com>
2019-07-10 17:55:35 +0800

    Pair program exercise

---
+++ account.js
@@ -0,0 +1,60 @@
+class Account {
+  constructor() {
+    this.balance = 0;
+    this.statement = "DATE\t\tAMOUNT\t\tBALANCE";
+  }
+
+  deposit(amount) {
+    this.balance += amount;
+    this.statement += `\n${Account.formatDate()}\t\t+${amount}\t\t${
+      this.balance
+    }`;
+
+    console.log(this.statement);
+  }
+
+  withdraw(amount) {
+    try {
+      if (amount <= 0) {
+        throw new Error("Cannot withdraw 0 or negative amount");
+      } else if (this.balance > amount) {
+        this.balance -= amount;
+        this.statement += `\n${Account.formatDate()}\t\t-${amount}\t\t${
+          this.balance
+        }`;
+      }
+
+      throw new Error("Withdrawn amount is greater than balance");
+    } catch (error) {
+      return error.message;
+    }
+
+    console.log(this.statement);
+  }
+
+  static formatDate() {
+    let today = new Date();
+    let dd = today.getDate();
+    let mm = today.getMonth() + 1; //January is 0!
+
+    const yyyy = today.getFullYear();
+    if (dd < 10) {
+      dd = "0" + dd;
+    }
+    if (mm < 10) {
+      mm = "0" + mm;
+    }
+    today = dd + "/" + mm + "/" + yyyy;
+    return today;
+  }
+
+  getBalance() {
+    return this.balance;
+  }
+
+  getStatement() {
+    return this.statement;
+  }
+}
+
+module.exports = Account;
--- test/account.test.js
+++ test/account.test.js
@@ -1,3 +1,49 @@
+const Account = require("../account");
+
+let peter;
+beforeEach(() => {
+  peter = new Account();
+});
+
 describe("account", () => {
-  it("should create an account object with balance property", () => {});
+  it("should create an account object with balance property", () => {
+    expect(peter.balance).toEqual(0);
+  });
+
+  it("should add money to balance with deposit", () => {
+    peter.deposit(10);
+    expect(peter.balance).toEqual(10);
+  });
+
+  it("should reduce amount from balance with withdrawal", () => {
+    peter.deposit(10);
+    peter.withdraw(5);
+    expect(peter.balance).toEqual(5);
+  });
+
+  it("should print balance with balance function", () => {
+    peter.deposit(10);
+    peter.withdraw(5);
+    expect(peter.getBalance()).toEqual(5);
+  });
+
+  it("should print statement with statement function", () => {
+    peter.deposit(10);
+    peter.deposit(5);
+    peter.withdraw(2);
+    expect(peter.getStatement()).toEqual(
+      `DATE\t\tAMOUNT\t\tBALANCE\n${Account.formatDate()}\t\t+10\t\t10\n${Account.formatDate()}\t\t+5\t\t15\n${Account.formatDate()}\t\t-2\t\t13`
+    );
+  });
+
+  it("should not withdraw more than available balance", () => {
+    peter.deposit(10);
+    expect(peter.withdraw(15)).toEqual(
+      "Withdrawn amount is greater than balance"
+    );
+  });
+
+  it("should not withdraw 0 or negative amount", () => {
+    expect(peter.withdraw(-10)).toEqual("Cannot withdraw 0 or negative amount");
+  });
 });
